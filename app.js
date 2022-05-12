42e138a77f424e83d8663f17611b7c1259fb462f
AbdulRehman <likeam99@gmail.com>
2022-05-12 11:28:37 +0500

    Building review section

--- app.js
+++ app.js
@@ -48,19 +48,38 @@ var swiper = new swiper(".product-slider", {
     },
     centeredSlides: true,
     breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1020: {
-        slidesPerView: 3,
-      },
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
     },
 });
 
-
+var swiper = new swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
 
 
 
